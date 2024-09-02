0fa8be3a6146fb6c0206ed19a253603568dbb82e
Yongjian Bai <byj6696@gmail.com>
2024-09-02 14:09:55 +0800

    create a static 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const NotFound: NextPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
